e1e13fd309acce42f663f6f013accc49b5d13031
Josh <119835493+josh76543210@users.noreply.github.com>
2025-02-04 20:32:23 -0800

    Set up components;

---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,7 @@
+import styles from "./ProfileCard.module.css";
+
+function ProfileCard() {
+  return <div></div>;
+}
+
+export default ProfileCard;
---
+++ src/components/ProfileLink/ProfileLink.jsx
@@ -0,0 +1,7 @@
+import styles from "./ProfileLink.module.css";
+
+function ProfileLink() {
+  return <div></div>;
+}
+
+export default ProfileLink;
