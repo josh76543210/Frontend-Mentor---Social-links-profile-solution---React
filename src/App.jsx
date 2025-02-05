f469ae32a21c9e946ad3df8181994725c97f69ea
Josh <119835493+josh76543210@users.noreply.github.com>
2025-02-04 19:08:06 -0800

    Initial commit;

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      Jessica Randall London, United Kingdom "Front-end developer and avid
+      reader." GitHub Frontend Mentor LinkedIn Twitter Instagram
+    </>
+  );
+}
+
+export default App;
